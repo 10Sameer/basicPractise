683a147e8c60fcd4db648dd4d9ab2d96b76d59f7
Sameer <bhandarisameer173@gmail.com>
2025-06-29 21:53:52 +0545

    basic form php

--- form.php
+++ form.php
@@ -11,6 +11,24 @@ if (isset($_SESSION['user_id'])) {
 $username = $email = $password = $confirm_password = "";
 $username_err = $email_err = $password_err = $confirm_password_err = "";
 
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    // Validate username
+    if (empty(trim($_POST["username"]))) {
+        $username_err = "Please enter a username.";
+    } else {
+        // Prepare a select statement
+        $sql = "SELECT id FROM Userdata WHERE username = ?";
+        
+        if ($stmt = $conn->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("s", $param_username);
+            
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+            
+           
 ?>
 
 <!DOCTYPE html>
